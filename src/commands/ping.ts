ba22a85d88b94f56d5face0a1de2e77663d16551
Jonny <jonnyschultze@hotmail.de>
2020-11-28 13:34:19 +0100

    added ping-cmd

---
+++ src/commands/ping.ts
@@ -0,0 +1,18 @@
+import { Command } from '../types'
+
+const Ping: Command = {
+  name: 'ping',
+  description: 'Ping-Pong!',
+  cooldown: 5,
+  usage: '',
+  args: false,
+  aliases: [],
+  guildOnly: false,
+  execute: (message, args): void => {
+    message.channel.send('Pong').catch((err) => {
+      console.log(err)
+    })
+  }
+}
+
+export default Ping
