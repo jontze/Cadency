c5bb907d533e2ac8ffcb7d03b1a201b56bfb5abc
UsingPython <jonnyschultze@hotmail.de>
2020-11-29 10:53:46 +0100

    add Slap-cmd

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,10 +1,12 @@
 import { Commands } from '../typings'
 import Ping from './ping'
 import Test from './test'
+import Slap from './slap'
 
 const commands: Commands = {
   [Ping.name]: Ping,
-  [Test.name]: Test
+  [Test.name]: Test,
+  [Slap.name]: Slap
 }
 
 export default commands
