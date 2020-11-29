59547f2587573dc8b70bebf2bb377e0a89424466
UsingPython <jonnyschultze@hotmail.de>
2020-11-29 20:54:59 +0100

    add inspire-cmd

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,12 +3,14 @@ import Ping from './ping'
 import Test from './test'
 import Slap from './slap'
 import Fib from './fib'
+import Inspire from './inspire'
 
 const commands: Commands = {
   [Ping.name]: Ping,
   [Test.name]: Test,
   [Slap.name]: Slap,
-  [Fib.name]: Fib
+  [Fib.name]: Fib,
+  [Inspire.name]: Inspire
 }
 
 export default commands
