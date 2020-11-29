5e1ed8deb60bf6decc621a7c1b9e78de2a307ce5
UsingPython <jonnyschultze@hotmail.de>
2020-11-29 20:31:07 +0100

    add fib-cmd

---
+++ src/commands/fib.ts
@@ -0,0 +1,27 @@
+import { Command } from '../typings'
+
+function fib (n: number): number {
+  const phi = (1 + Math.sqrt(5)) / 2
+  const asymp = Math.pow(phi, n) / Math.sqrt(5)
+  return Math.round(asymp)
+}
+
+const Fib: Command = {
+  name: 'fib',
+  description: 'Calculate the nth Number in the Fibonacci Series',
+  args: true,
+  usage: '<n>',
+  guildOnly: true,
+  cooldown: 2,
+  aliases: [],
+  execute (message, args) {
+    const inputNumber = parseFloat(args[0])
+    if (!isNaN(inputNumber)) {
+      message.channel.send(fib(inputNumber)).catch((err) => console.log(err))
+    } else {
+      message.channel.send(`Is \`${args[0]}\` really a valid number?`).catch((err) => console.log(err))
+    }
+  }
+}
+
+export default Fib
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,11 +2,13 @@ import { Commands } from '../typings'
 import Ping from './ping'
 import Test from './test'
 import Slap from './slap'
+import Fib from './fib'
 
 const commands: Commands = {
   [Ping.name]: Ping,
   [Test.name]: Test,
-  [Slap.name]: Slap
+  [Slap.name]: Slap,
+  [Fib.name]: Fib
 }
 
 export default commands
