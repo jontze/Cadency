23841d13b61ead213d23987c160902950ece598f
UsingPython <jonnyschultze@hotmail.de>
2020-11-30 02:11:48 +0100

    Add urban-cmd

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -4,13 +4,15 @@ import Test from './test'
 import Slap from './slap'
 import Fib from './fib'
 import Inspire from './inspire'
+import Urban from './urban'
 
 const commands: Commands = {
   [Ping.name]: Ping,
   [Test.name]: Test,
   [Slap.name]: Slap,
   [Fib.name]: Fib,
-  [Inspire.name]: Inspire
+  [Inspire.name]: Inspire,
+  [Urban.name]: Urban
 }
 
 export default commands
---
+++ src/commands/urban.ts
@@ -0,0 +1,46 @@
+import querystring from 'query-string'
+import axios from 'axios'
+import { MessageEmbed } from 'discord.js'
+import { Command, UrbanDictionary } from '../typings'
+
+const trim = (str: string, max: number): string => (str.length > max ? `${str.slice(0, max - 3)}...` : str)
+
+const Urban: Command = {
+  name: 'urban',
+  description: 'Searches the Urbandictionary for the user input',
+  guildOnly: true,
+  aliases: ['ud', 'dictionary'],
+  cooldown: 10,
+  args: true,
+  usage: '<word>',
+  execute (message, args) {
+    // Create Querystring
+    const query = querystring.stringify({ term: args.join(' ') })
+    // Send request
+    axios.get(`https://api.urbandictionary.com/v0/define?${query}`).then((result) => {
+      const firstMatch: UrbanDictionary | undefined = result.data.list[0]
+      // Check if list has entrys
+      if (firstMatch === undefined) {
+        message.channel.send(`Kein Eintrag für **${args.join(' ')}**.`).catch((err) => console.log(err))
+      } else {
+        // Create, style and send message
+        const embed = new MessageEmbed()
+        embed
+          .setColor('#EFFF00')
+          .setTitle(trim(firstMatch.word, 256))
+          .setURL(firstMatch.permalink)
+          .addFields(
+            { name: 'Definition', value: trim(firstMatch.definition, 1024) },
+            { name: 'Example', value: trim(firstMatch.example, 1024) },
+            { name: 'Rating', value: `${firstMatch.thumbs_up} :thumbsup: \n ${firstMatch.thumbs_down} :thumbsdown:` }
+          )
+        message.channel.send(embed).catch((err) => console.log(err))
+      }
+    }).catch((err) => {
+      console.log(err)
+      message.channel.send('Shit just hits the fan...').catch((err) => console.log(err))
+    })
+  }
+}
+
+export default Urban
