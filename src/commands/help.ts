eb67a1ab8417033288013f8c108ef66168d4b425
Jonny <jonnyschultze@hotmail.de>
2020-12-02 23:37:31 +0100

    removed wrong spaces

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -18,7 +18,7 @@ const Help: Command = {
     for (const key in cmd) {
       if (Object.prototype.hasOwnProperty.call(cmd, key)) {
         const element = cmd[key]
-        embed.addField(`${position}. ${element.description}`, `${element.name} ${element.usage}\n cooldown: ${element.cooldown} ${element.aliases.length === 0 ? '' : `\n aliases: ${element.aliases.join(', ')}`}`)
+        embed.addField(`${position}. ${element.description}`, `${element.name} ${element.usage}\ncooldown: ${element.cooldown} ${element.aliases.length === 0 ? '' : `\naliases: ${element.aliases.join(', ')}`}`)
         position = position + 1
       }
     }
