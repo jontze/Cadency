2db93ddad327897ad9b431be28c21e53f5461cfe
Jonny <jonnyschultze@hotmail.de>
2020-12-03 13:09:16 +0100

    moved basic functions to Base-Class

--- src/bot/Bot.ts
+++ src/bot/Bot.ts
@@ -1,39 +1,11 @@
-import { Collection, Message, Client, MessageEmbed } from 'discord.js'
+import { Message, MessageEmbed } from 'discord.js'
 import ytdl from 'ytdl-core'
 
-import { BotInterface as BotI, BotConfig, Commands, Command, QueueSong } from '../typings'
-import commands from '../commands'
+import Base from './Base'
+import { BotInterface as BotI, QueueSong } from '../typings'
 import logger from '../logger'
 
-export default class DiscordBot implements BotI {
-  private readonly token: string
-  private readonly client: Client
-  private readonly commands: Commands
-  private readonly cooldowns: Collection<string, Collection<string, number>>
-  private readonly songList: Collection<string, QueueSong>
-  private readonly config: BotConfig = {
-    prefix: '/',
-    activity: 'Listen to some music',
-    activityType: 'CUSTOM_STATUS',
-    status: 'online'
-  }
-
-  constructor (token: string, config?: BotConfig) {
-    this.token = token
-    if (config !== undefined) this.setConfig(config)
-    this.commands = commands
-    this.client = new Client()
-    this.cooldowns = new Collection()
-    this.songList = new Collection()
-  }
-
-  private setConfig (config: BotConfig): void {
-    this.config.prefix = config.prefix ?? this.config.prefix
-    this.config.activity = config.activity ?? this.config.activity
-    this.config.activityType = config.activityType ?? this.config.activityType
-    this.config.status = config.status ?? this.config.status
-  }
-
+export default class DiscordBot extends Base implements BotI {
   public start (): void {
     this.client.login(this.token).then(() => {
       logger.info('Start awesome things...')
@@ -98,66 +70,6 @@ export default class DiscordBot implements BotI {
     } catch (e) {
       message.reply('There was an error trying to execute that command!').catch((err) => logger.error(err))
     }
-  }
-
-  private parseArgsAndCommand (message: Message): [string[], string] {
-    const args = message.content.slice(this.config.prefix.length).split(/ +/).slice(1)
-    const commandName = message.content.slice(this.config.prefix.length).split(/ +/)[0].toLowerCase()
-    logger.info(`Command: ${commandName}, args: ${args.join(' ')}`)
-    return [args, commandName]
-  }
-
-  private getCommand (name: string): Command | undefined {
-    const command: Command | undefined = this.commands[name] ?? this.searchAliases(name)
-    return command
-  }
-
-  private searchAliases (name: string): Command | undefined {
-    const command = undefined
-    for (const key in this.commands) {
-      if (this.commands[key].aliases.includes(name)) {
-        return this.commands[key]
-      }
-    }
-    return command
-  }
-
-  private checkGuildOnly (command: Command, message: Message): void {
-    if (command.guildOnly && message.channel.type !== 'text') {
-      message.reply('I can\'t execute that command inside DMs!').catch((err) => logger.error(err))
-    }
-  }
-
-  private checkArgsRequired (command: Command, args: string[], message: Message): void {
-    if (command.args && args.length === 0) {
-      let reply = `You didn't provide any arguments, @${message.author.toString()}!`
-      reply += `\nThe proper usage would be: \`${this.config.prefix}${command.name} ${command.usage}\``
-      message.channel.send(reply).catch((err) => logger.error(err))
-    }
-  }
-
-  private hasCooldown (command: Command, message: Message): boolean {
-    // Add Command to Collection if never used
-    if (!this.cooldowns.has(command.name)) {
-      this.cooldowns.set(command.name, new Collection())
-    }
-    const now = Date.now()
-    const cooldownAmount = command.cooldown * 1000
-    // Check if Author already in collection
-    if (this.cooldowns.get(command.name)?.has(message.author.id) === true) {
-      // Check if cooldown passed
-      const expirationTime = (this.cooldowns.get(command.name)?.get(message.author.id) as number) + cooldownAmount
-      if (now < expirationTime) {
-        const timeLeft = (expirationTime - now) / 1000
-        message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`).catch((err) => logger.error(err))
-        return true
-      }
-    }
-    // Set Timestamp in cooldown collection
-    this.cooldowns.get(command.name)?.set(message.author.id, now)
-    // Delete entry after cooldown
-    setTimeout(() => this.cooldowns.get(command.name)?.delete(message.author.id), cooldownAmount)
-    return false
   }
 
   private songHandler (song: QueueSong, guildId: string, message: Message): void {
