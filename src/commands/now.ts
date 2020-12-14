cc7ae1fbada523e2b3137ecf415bad43c892617f
UsingPython <jonnyschultze@hotmail.de>
2020-12-14 21:32:43 +0100

    add now-cmd

---
+++ src/commands/now.ts
@@ -0,0 +1,18 @@
+import { Command } from '../typings'
+
+const Now: Command = {
+  name: 'now',
+  description: 'Show current song.',
+  args: false,
+  cooldown: 5,
+  usage: '',
+  aliases: [],
+  guildOnly: true,
+  execute (message, args) {
+    const voiceChannelUser = message.member?.voice.channel
+
+    message.client.emit('currentSong', voiceChannelUser?.guild.id, message)
+  }
+}
+
+export default Now
