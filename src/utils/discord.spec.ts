73bc0881e949cf1904763e22d33e567d4479d753
UsingPython <42588836+UsingPython@users.noreply.github.com>
2021-05-28 13:28:05 +0200

    refactor(utils): Move common used discordjs functionalities to utils

---
+++ src/utils/discord.spec.ts
@@ -0,0 +1,183 @@
+import { Role } from ".prisma/client";
+import { Client, Message, TextChannel } from "discord.js";
+import { CommandVoiceError } from "../errors/command";
+import messageContent from "../message-content";
+import { Command } from "../typings";
+import {
+  getGuildId,
+  isNewPrefixValid,
+  validateCommand,
+  validateVoiceCommand,
+} from "./discord";
+
+// Discord mocks
+const mockPermissionsForChannel = jest.fn();
+let mockVoiceId = "123";
+let mockGuildId = "4321";
+let mockChannelType = "text";
+const mockClientUser: string | null = "UserObj";
+const mockHasPermission = jest.fn();
+
+jest.mock("discord.js", () => {
+  return {
+    Message: jest.fn().mockImplementation(() => {
+      return {
+        member: {
+          voice: {
+            channel: {
+              permissionsFor: mockPermissionsForChannel,
+              id: mockVoiceId,
+            },
+          },
+          hasPermission: mockHasPermission,
+        },
+        client: {
+          user: mockClientUser,
+        },
+        channel: {
+          type: mockChannelType,
+        },
+        guild: {
+          id: mockGuildId,
+        },
+      };
+    }),
+  };
+});
+
+describe("Discord Utils", () => {
+  let msg: Message;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    msg = new Message(
+      {} as unknown as Client,
+      {},
+      "" as unknown as TextChannel
+    );
+  });
+
+  it("should extract guild id", () => {
+    expect.assertions(1);
+    const guildId = getGuildId(msg);
+    expect(guildId).toBe(mockGuildId);
+  });
+
+  it("should validate command", () => {
+    expect.assertions(2);
+    mockHasPermission.mockReturnValue(true);
+    let command = {
+      permission: Role.ADMIN,
+      guildOnly: true,
+      args: true,
+    } as unknown as Command;
+    msg.channel.type = "text";
+
+    expect(() => validateCommand(command, ["test"], msg)).not.toThrowError();
+    expect(mockHasPermission).toHaveBeenCalled();
+  });
+
+  it("should try to validate command and throw args error", () => {
+    expect.assertions(2);
+    mockHasPermission.mockReturnValue(true);
+    let command = {
+      permission: Role.ADMIN,
+      guildOnly: true,
+      args: true,
+    } as unknown as Command;
+    msg.channel.type = "text";
+
+    expect(() => validateCommand(command, [], msg)).toThrowError(
+      messageContent.error.command.noArgs
+    );
+    expect(mockHasPermission).not.toHaveBeenCalled();
+  });
+
+  it("should try to validate command and throw guild error", () => {
+    expect.assertions(2);
+    mockHasPermission.mockReturnValue(true);
+    let command = {
+      permission: Role.ADMIN,
+      guildOnly: true,
+      args: true,
+    } as unknown as Command;
+    msg.channel.type = "dm";
+
+    expect(() => validateCommand(command, ["test"], msg)).toThrowError(
+      messageContent.error.command.guildOnly
+    );
+    expect(mockHasPermission).not.toHaveBeenCalled();
+  });
+
+  it("should try to validate command and throw permission error", () => {
+    expect.assertions(2);
+
+    mockHasPermission.mockReturnValue(false);
+    let command = {
+      permission: Role.ADMIN,
+      guildOnly: true,
+      args: true,
+    } as unknown as Command;
+    msg.channel.type = "text";
+
+    expect(() => validateCommand(command, ["test"], msg)).toThrowError(
+      messageContent.error.command.noPermission
+    );
+    expect(mockHasPermission).toHaveBeenCalled();
+  });
+
+  it("should validate voice channel", () => {
+    expect.assertions(3);
+    mockHasPermission.mockReturnValue(true);
+    mockPermissionsForChannel.mockReturnValueOnce({ has: () => true });
+    let command = {
+      permission: Role.ADMIN,
+      guildOnly: true,
+      args: true,
+    } as unknown as Command;
+    msg.channel.type = "text";
+
+    expect(() =>
+      validateVoiceCommand(command, ["test"], msg)
+    ).not.toThrowError();
+    expect(mockHasPermission).toHaveBeenCalled();
+    expect(mockPermissionsForChannel).toHaveBeenCalledWith(mockClientUser);
+  });
+
+  it("should throw CommandVoiceError on invalid voice channel", () => {
+    expect.assertions(3);
+    mockHasPermission.mockReturnValue(true);
+    mockPermissionsForChannel.mockReturnValueOnce({ has: () => false });
+    let command = {
+      permission: Role.ADMIN,
+      guildOnly: true,
+      args: true,
+    } as unknown as Command;
+    msg.channel.type = "text";
+    try {
+      validateVoiceCommand(command, ["test"], msg);
+    } catch (e) {
+      expect(mockHasPermission).toHaveBeenCalled();
+      expect(mockPermissionsForChannel).toBeCalledWith(mockClientUser);
+      expect(e instanceof CommandVoiceError).toBe(true);
+    }
+  });
+
+  it("should validate matching new prefix", () => {
+    expect.assertions(1);
+    const isValid = isNewPrefixValid("!");
+    expect(isValid).toBe(true);
+  });
+
+  it("should reject new prefix due to length", () => {
+    expect.assertions(1);
+    const isValid = isNewPrefixValid("!!!!!!!!!");
+    expect(isValid).toBe(false);
+  });
+
+  it("should reject new prefix due to invalid char", () => {
+    expect.assertions(1);
+    const isValid = isNewPrefixValid("a");
+    expect(isValid).toBe(false);
+  });
+});
