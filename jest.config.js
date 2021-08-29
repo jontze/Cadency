913986aed07c284130faf87e3e23f4fe72b96c58
Jontze <42588836+jontze@users.noreply.github.com>
2021-08-30 00:21:32 +0200

    chore: Fix jest testsuite to run only on ts-files

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testMatch: ["**/?(*.)+(spec).[jt]s?(x)"],
+  testMatch: ["**/?(*.)+(spec).ts?(x)"],
   collectCoverage: true,
   collectCoverageFrom: ["src/**/*.ts", "!src/main.ts"],
   coveragePathIgnorePatterns: ["node_modules", "typings", "logger", "errors"],
