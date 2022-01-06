88dd7602154918ea40e5cc975171f982f45fb802
Jontze <42588836+jontze@users.noreply.github.com>
2022-01-06 23:32:30 +0100

    tmp

--- src/commands/fib.ts
+++ src/commands/fib.ts
@@ -2,12 +2,23 @@ import { Command } from "../typings";
 import { Role } from ".prisma/client";
 import { CommandArgsError } from "../errors/command";
 import messageContent from "../message-content";
+import { SlashCommandBuilder } from "@discordjs/builders";
 
 function fib(n: number): number {
   const phi = (1 + Math.sqrt(5)) / 2;
   const asymp = Math.pow(phi, n) / Math.sqrt(5);
   return Math.round(asymp);
 }
+
+const Fib2 = new SlashCommandBuilder()
+  .setName("fib")
+  .setDescription("Calculate the nth Number in the Fibonacci Series")
+  .addIntegerOption((option) =>
+    option
+      .setName("number")
+      .setRequired(true)
+      .setDescription("Input number to calculate")
+  );
 
 /**
  * Command to calculate the nth position in the fibonacci series
@@ -22,6 +33,8 @@ const Fib: Command = {
   aliases: [],
   permission: Role.MEMBER,
   execute: async (message, args): Promise<void> => {
+    const test = new SlashCommandBuilder();
+
     if (args[0] == null) throw new CommandArgsError("Args undefined");
     const inputNumber = parseFloat(args[0]);
     if (!isNaN(inputNumber)) {
